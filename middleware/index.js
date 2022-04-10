8fa25dbb0f2c20c64ff44eaaf7d0a4d2c5371f9e
KJ Jackson <kjjackson619@yahoo.com>
2022-04-09 20:29:11 -0400

    middleware

---
+++ middleware/index.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+
+const router = express.Router();
+
+const getPath = (name) => path.join(__dirname, '..', 'public', `${name}.html`);
+
+
+router.get('/notes', (req, res) => {
+    res.sendFile(getPath('notes'));
+});
+
+
+router.get('*', (req, res) => {
+    res.sendFile(getPath('index'));
+});
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+
+const router = express.Router();
+
+const getPath = (name) => path.join(__dirname, '..', 'public', `${name}.html`);
 
 const app = express();
 
@@ -7,11 +12,18 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api/notes', require('./middleware/api'));
-
-app.use('/', require('./middleware/html'));
-
 const PORT = process.env.PORT || 3001;
+
+
+
+router.get('/notes', (req, res) => {
+    res.sendFile(getPath('notes'));
+});
+
+
+router.get('*', (req, res) => {
+    res.sendFile(getPath('index'));
+});
 
 //const { notes } = require('./db/db.json');
 
