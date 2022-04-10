8230e9733fec0d7d5fd1941e685ff2f63161ad2c
KJ Jackson <kjjackson619@yahoo.com>
2022-04-09 20:39:31 -0400

    server

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
 
-const router = express.Router();
-
-const getPath = (name) => path.join(__dirname, '..', 'public', `${name}.html`);
 
 const app = express();
 
@@ -12,29 +8,13 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+app.use('api/notes', require('./middleware/api'));
+
+app.use('/', require('./middleware/index'));
+
 const PORT = process.env.PORT || 3001;
 
-
-
-router.get('/notes', (req, res) => {
-    res.sendFile(getPath('notes'));
-});
-
-
-router.get('*', (req, res) => {
-    res.sendFile(getPath('index'));
-});
-
-//const { notes } = require('./db/db.json');
-
-// app.get('/api/notes', (req, res) => {
-
-//     let results = notes;
-
-//     console.log(req.query)
-
-//     res.json(results);
-// });
 
 
 app.listen(3001, () => {
